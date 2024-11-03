f98f6cbdb99d3685558d0b64fe6a162980130b31
Diki Faturrohman <118197085+DikiFaturrohman@users.noreply.github.com>
2024-11-03 18:48:44 +0700

    Blade Template

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -3,12 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     @vite('resources/css/app.css')
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <title>Halaman Home</title>
-
 </head>
 <body class="h-full">
 <div class="min-h-full">
@@ -112,13 +110,13 @@
     </nav>
     <header class="bg-white shadow">
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home Page</h1>
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Blog</h1>
         </div>
     </header>
     <main>
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <!-- Your content -->
-         <p>Welcome to my home page!</p>
+         <p>Welcome to my blog page!</p>
         </div>
     </main>
 </div>
